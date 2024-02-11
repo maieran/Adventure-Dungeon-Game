4c83f277acb9f3a7096b32f6d9e02443c17c59f8
maieran <apak_mc@hotmail.de>
2024-02-11 19:29:35 +0100

    adding JavaFX libraries and GameGui class

---
+++ src/de/game/modules/GameGui.java
@@ -0,0 +1,22 @@
+package de.game.modules;
+
+
+
+import javafx.scene.image.ImageView;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+
+
+public class GameGui extends Application
+{
+    private ImageView playerIcon;
+    private ImageView enemyIcon;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
