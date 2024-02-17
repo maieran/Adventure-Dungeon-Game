11f7016fb6280d6623272b839a82ad81708afc66
maieran <apak_mc@hotmail.de>
2024-02-17 12:40:43 +0100

    adding functional gui javaFx classes

--- src/de/game/modules/GameClass.java
+++ src/de/game/modules/GameClass.java
@@ -15,6 +15,118 @@ import java.util.Scanner;
 
 public class GameClass {
     private static boolean isFightInProgress = false;
+    public GameGui gameGui;
+
+    public GameClass() {
+        this.gameGui = new GameGui();
+    }
+    public void startGame() {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+
+        // Player character
+        Bag standardPlayerBag = new Bag(10, "BagImage");
+        Weapon standardShortPlayerSword =  new Weapon("Sword", true, false, random.nextInt(31), 0, WeaponType.SHORT_SWORD);
+        Inventory standardPlayerInventory = new Inventory();
+        standardPlayerBag.addInventoryObject(standardShortPlayerSword);
+        standardPlayerInventory.addBag(standardPlayerBag);
+
+        PlayerCharacter player = new PlayerCharacter(0, 100, "Player", 25, 26, 4, standardPlayerBag,
+                standardShortPlayerSword, standardPlayerInventory);
+        player.setAttackDamage(player.getAttackDamage() + standardShortPlayerSword.getAttack());
+        player.setBag(standardPlayerBag);
+        for (int i = 1; i <= 4; i++) {
+            player.getBag().addInventoryObject(new Potion("Health Potion 🧪", 30));
+        }
+
+
+
+        System.out.println("WELCOME TO THE DUNGEON");
+
+        while (true) {
+            System.out.println("*************************************************");
+            AbstractCharacter[] enemiesCharacters = {new Skeleton(1), new Zombie(2), new Warrior(3), new Assassin(4)};
+            AbstractCharacter enemy = enemiesCharacters[random.nextInt(enemiesCharacters.length)];
+            System.out.println("\t<< " + enemy.getName() + " has appeared ! >>\n");
+
+            GAME:
+            while (enemy.getHealth() > 0 && player.getHealth() > 0) {
+                System.out.println("\tYour HP: " + player.getHealth());
+                System.out.println("\t" + enemy.getName() + "'s HP: " + enemy.getHealth());
+
+                //TODO: Bring another category that can gives you a pet, where you can decide either the pet or you can receive the dmg for Warrior and can heal itself
+                //TODO: Add some loot for Skeleton and Zombie
+
+                gameGui.showIcons();
+
+                /**
+                 * Checks if assasineGoogle is equipped, not null and uses it then on the player and the enemy
+                 */
+                useEquippedAssassinGoogle(enemy, player);
+
+
+                System.out.println("\n\tWhat would you like to do with your enemy?");
+                System.out.println("\t1. Attack");
+                System.out.println("\t2. Drink health potion");
+                System.out.println("\t3. Run!!");
+                System.out.println("\t4. Open inventory");
+
+                // Interaction with the player/user
+                String input = scanner.nextLine();
+                switch (input) {
+                    case "1":
+
+                        fightingTheEnemy(player, enemy);
+
+                        break;
+                    case "2":
+                        player.useHealthPotion();
+                        break;
+                    case "3":
+                        System.out.println("\t> You run away from the " + enemy.getName() + "!");
+                        break GAME;
+                    case "4":
+                        gameGui.hideIcons();
+                        System.out.println("You have opened inventory");
+                        player.openBag(scanner); //later u may have multiple bags
+                        break;
+                    default:
+                        System.out.println("Invalid command, Choose 1, 2, 3, or 4!");
+                }
+
+                // Check after interaction whether the player is still alive
+                if (player.getHealth() < 1) {
+                    System.out.println("You are bleeding out and cannot fight anymore");
+                    break;
+                }
+
+
+
+                System.out.println("*************************************************");
+                System.out.println(" What would you like to do now? ");
+                System.out.println("1. Continue fighting? ");
+                System.out.println("2. Exit game");
+                input = scanner.nextLine();
+
+                while (!input.equals("1") && !input.equals("2")) {
+                    System.out.println("Invalid command! Choose 1 or 2");
+                    input = scanner.nextLine();
+                }
+                if (input.equals("1")) {
+                    System.out.println("You can continue on your adventure!");
+                } else if (input.equals("2")) {
+                    System.out.println("Game over");
+                    break;
+                }
+            }
+
+            if (player.getHealth() < 0 ) {
+                System.out.println("You are dead, game over !");
+                return;
+            }
+        }
+    }
 
     public void startFight() {
         isFightInProgress = true;
@@ -29,6 +141,110 @@ public class GameClass {
     }
 
     public static void main(String[] args) {
+        GAMELOOP();
+    }
+
+    private static void fightingTheEnemy(PlayerCharacter player, AbstractCharacter enemy) {
+        int damageDealt = player.dealDamage(player.getAttackDamage());
+        int reducedDamageDealt = enemy.defendDamage(damageDealt);
+        enemy.takeDamage(reducedDamageDealt);
+        System.out.println("\t You strike the " + enemy.getName() + " for " + damageDealt + " damage. ");
+
+        if (reducedDamageDealt > 0) {
+            System.out.println("\t The " + enemy.getName() + " attempts to defend, but receives " + reducedDamageDealt + " damage.");
+        } else {
+            System.out.println("\t The " + enemy.getName() + " was able to defend and there is no impact");
+        }
+
+
+        int damageTaken = enemy.dealDamage(enemy.getAttackDamage());
+        int reduceDamageTaken = player.defendDamage(damageTaken);
+        player.takeDamage(reduceDamageTaken);
+
+        if (reduceDamageTaken > 0) {
+            System.out.println("\t The " + player.getName() + " attempts to defend, but receives " + reduceDamageTaken  + " damage.");
+        } else {
+            System.out.println("\t The " + player.getName() + " was able to defend the attack from " + enemy.getName() + " there is no impact");
+        }
+        System.out.println(" ");
+
+        handleEnemyDefeat(enemy, player);
+
+        if (player.getHealth() <= 0) {
+            System.out.println("\t You have taken too much damage, you are too weak and die");
+        }
+    }
+
+    private static void useEquippedAssassinGoogle(AbstractCharacter enemy, PlayerCharacter player) {
+        if (player.isEquippedWith(InventoryObjectType.ASSASSIN_GOOGLE)) {
+            Optional<InventoryObject> assassinGoogleOnOptional = player.getEquippedItems().stream()
+                    .filter(obj -> obj.getInventoryObjectType() == InventoryObjectType.ASSASSIN_GOOGLE)
+                    .findFirst();
+
+            if (assassinGoogleOnOptional.isPresent()) {
+                AssassinGoogle assassinGoogleOnUse = (AssassinGoogle) assassinGoogleOnOptional.get();
+                if (assassinGoogleOnUse.isEquiped()) {
+                    assassinGoogleOnUse.readAttackDamage(enemy, player);
+                }
+            }
+        }
+    }
+
+    private static void handleEnemyDefeat(AbstractCharacter enemy, PlayerCharacter player) {
+        Scanner scanner = new Scanner(System.in);
+        if (enemy.isDefeated()) {
+            System.out.println("*************************************************");
+            System.out.println(" ⚔︎ " + enemy.getName() + " was defeated! ⚔︎ ");
+            if (enemy.shouldDropPotion()) {
+                System.out.println(" ⚔︎ The " + enemy.getName() + " dropped a health potion 🧪 ⚔︎ ");
+
+                System.out.println("*************************************************");
+                System.out.println("Do you want to loot the health potion 🧪?");
+                System.out.println("1. Yes, I want to pick it up");
+                System.out.println("2. Leave it, as it is.");
+                String input = scanner.nextLine();
+
+                while (!input.equals("1") && !input.equals("2")) {
+                    System.out.println("Invalid command! Choose 1 or 2");
+                    input = scanner.nextLine();
+                }
+
+                if (input.equals("1")) {
+                    agreeToAddLootedHealthPotionToBag(player);
+                }
+
+                if (input.equals("2")) {
+                    System.out.println("You are leaving the dead body of " + enemy.getName() + " to the worms");
+                }
+
+                checkIfAssassinGooglesAreDropped(enemy, player);
+            }
+        }
+        System.out.println(" ⚔︎ You have " + player.getHealth() + " HP left. ⚔︎");
+    }
+
+    private static void agreeToAddLootedHealthPotionToBag(PlayerCharacter player) {
+        if (player.getBag().getSlots().size() < player.getBag().getSize()) {
+            System.out.println("You are picking up the health potion 🧪");
+            player.getBag().addInventoryObject(new Potion("Health Potion 🧪", 30));
+            int totalFreeSlotsInTheBag = player.getBag().utilTotalFreeSlotsInTheCurrentBag();
+            int totalAmountOfHealthPotionsInTheBag = player.getBag().utilTotalAmountOfHealthPotionsInTheBag(player.getBag());
+            System.out.println(" ︎ You have now " + totalAmountOfHealthPotionsInTheBag
+                    + " health potions 🧪 ⚔︎ ");
+            System.out.println(" You have " + totalFreeSlotsInTheBag + " free slots in your bag.");
+        } else {
+            System.out.println("The Bag is full, you need to free slots");
+        }
+    }
+
+    private static void checkIfAssassinGooglesAreDropped(AbstractCharacter enemy, PlayerCharacter player) {
+        if (enemy.isDefeated() && enemy instanceof Assassin) {
+            ((Assassin) enemy).onLoot(player);
+        }
+    }
+
+
+    public static void GAMELOOP() {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
@@ -132,105 +348,4 @@ public class GameClass {
             }
         }
     }
-
-    private static void fightingTheEnemy(PlayerCharacter player, AbstractCharacter enemy) {
-        int damageDealt = player.dealDamage(player.getAttackDamage());
-        int reducedDamageDealt = enemy.defendDamage(damageDealt);
-        enemy.takeDamage(reducedDamageDealt);
-        System.out.println("\t You strike the " + enemy.getName() + " for " + damageDealt + " damage. ");
-
-        if (reducedDamageDealt > 0) {
-            System.out.println("\t The " + enemy.getName() + " attempts to defend, but receives " + reducedDamageDealt + " damage.");
-        } else {
-            System.out.println("\t The " + enemy.getName() + " was able to defend and there is no impact");
-        }
-
-
-        int damageTaken = enemy.dealDamage(enemy.getAttackDamage());
-        int reduceDamageTaken = player.defendDamage(damageTaken);
-        player.takeDamage(reduceDamageTaken);
-
-        if (reduceDamageTaken > 0) {
-            System.out.println("\t The " + player.getName() + " attempts to defend, but receives " + reduceDamageTaken  + " damage.");
-        } else {
-            System.out.println("\t The " + player.getName() + " was able to defend the attack from " + enemy.getName() + " there is no impact");
-        }
-        System.out.println(" ");
-
-        handleEnemyDefeat(enemy, player);
-
-        if (player.getHealth() <= 0) {
-            System.out.println("\t You have taken too much damage, you are too weak and die");
-        }
-    }
-
-    private static void useEquippedAssassinGoogle(AbstractCharacter enemy, PlayerCharacter player) {
-        if (player.isEquippedWith(InventoryObjectType.ASSASSIN_GOOGLE)) {
-            Optional<InventoryObject> assassinGoogleOnOptional = player.getEquippedItems().stream()
-                    .filter(obj -> obj.getInventoryObjectType() == InventoryObjectType.ASSASSIN_GOOGLE)
-                    .findFirst();
-
-            if (assassinGoogleOnOptional.isPresent()) {
-                AssassinGoogle assassinGoogleOnUse = (AssassinGoogle) assassinGoogleOnOptional.get();
-                if (assassinGoogleOnUse.isEquiped()) {
-                    assassinGoogleOnUse.readAttackDamage(enemy, player);
-                }
-            }
-        }
-    }
-
-    private static void handleEnemyDefeat(AbstractCharacter enemy, PlayerCharacter player) {
-        Scanner scanner = new Scanner(System.in);
-        if (enemy.isDefeated()) {
-            System.out.println("*************************************************");
-            System.out.println(" ⚔︎ " + enemy.getName() + " was defeated! ⚔︎ ");
-            if (enemy.shouldDropPotion()) {
-                System.out.println(" ⚔︎ The " + enemy.getName() + " dropped a health potion 🧪 ⚔︎ ");
-
-                System.out.println("*************************************************");
-                System.out.println("Do you want to loot the health potion 🧪?");
-                System.out.println("1. Yes, I want to pick it up");
-                System.out.println("2. Leave it, as it is.");
-                String input = scanner.nextLine();
-
-                while (!input.equals("1") && !input.equals("2")) {
-                    System.out.println("Invalid command! Choose 1 or 2");
-                    input = scanner.nextLine();
-                }
-
-                if (input.equals("1")) {
-                    agreeToAddLootedHealthPotionToBag(player);
-                }
-
-                if (input.equals("2")) {
-                    System.out.println("You are leaving the dead body of " + enemy.getName() + " to the worms");
-                }
-
-                checkIfAssassinGooglesAreDropped(enemy, player);
-            }
-        }
-        System.out.println(" ⚔︎ You have " + player.getHealth() + " HP left. ⚔︎");
-    }
-
-    private static void agreeToAddLootedHealthPotionToBag(PlayerCharacter player) {
-        if (player.getBag().getSlots().size() < player.getBag().getSize()) {
-            System.out.println("You are picking up the health potion 🧪");
-            player.getBag().addInventoryObject(new Potion("Health Potion 🧪", 30));
-            int totalFreeSlotsInTheBag = player.getBag().utilTotalFreeSlotsInTheCurrentBag();
-            int totalAmountOfHealthPotionsInTheBag = player.getBag().utilTotalAmountOfHealthPotionsInTheBag(player.getBag());
-            System.out.println(" ︎ You have now " + totalAmountOfHealthPotionsInTheBag
-                    + " health potions 🧪 ⚔︎ ");
-            System.out.println(" You have " + totalFreeSlotsInTheBag + " free slots in your bag.");
-        } else {
-            System.out.println("The Bag is full, you need to free slots");
-        }
-    }
-
-    private static void checkIfAssassinGooglesAreDropped(AbstractCharacter enemy, PlayerCharacter player) {
-        if (enemy.isDefeated() && enemy instanceof Assassin) {
-            ((Assassin) enemy).onLoot(player);
-        }
-    }
-
-
 }
--- src/de/game/modules/GameGui.java
+++ src/de/game/modules/GameGui.java
@@ -2,9 +2,13 @@ package de.game.modules;
 
 
 
+import de.game.modules.model.*;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 import javafx.application.Application;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
@@ -14,9 +18,73 @@ public class GameGui extends Application
 {
     private ImageView playerIcon;
     private ImageView enemyIcon;
+    private AbstractCharacter enemy;
 
     @Override
     public void start(Stage stage) throws Exception {
 
+        initializeIcons();
+
+        VBox layout = new VBox();
+        layout.getChildren().addAll(playerIcon,enemyIcon);
+
+        Scene scene = new Scene(layout, 400, 300);
+
+        stage.setTitle("Dungeon Game");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private void initializeIcons() {
+        //String currentWorkDir = System.getProperty("user.dir");
+        playerIcon = new ImageView(getClass().getResource("Images/swordman.png").toExternalForm());
+
+        //playerIcon.setX(100);
+        //playerIcon.setY(100);
+
+                //new Image("/src/de/game/modules/Images/swordman.png"));
+
+        //TODO:implement for all possible enemies
+        //new ImageView(new Image(getEnemyIconPath(enemy)));
+        enemyIcon = new ImageView(getClass().getResource("Images/skeleton.png").toExternalForm());
+
+
+
+        playerIcon.setVisible(false);
+        enemyIcon.setVisible(false);
+
+    }
+
+    private String getEnemyIconPath(AbstractCharacter enemy) {
+        //TODO: change to sys-env variable
+        if (enemy instanceof Skeleton) {
+            return "Images/skeleton.png";
+        } else if (enemy instanceof Zombie) {
+            return "/Users/PEAQ/Desktop/Own Projects/Adventure-Dungeon-Game/src/de/game/modules/Images/zombie.png";
+        } else if (enemy instanceof Warrior) {
+            return "/Users/PEAQ/Desktop/Own Projects/Adventure-Dungeon-Game/src/de/game/modules/Images/warrior.png";
+        } else if (enemy instanceof Assassin) {
+            return "/Users/PEAQ/Desktop/Own Projects/Adventure-Dungeon-Game/src/de/game/modules/Images/assassin.png";
+        } else {
+
+            return "/Users/PEAQ/Desktop/Own Projects/Adventure-Dungeon-Game/src/de/game/modules/Image/swordman.png";
+        }
+    }
+
+
+    public void showIcons() {
+        playerIcon.setVisible(true);
+        enemyIcon.setVisible(true);
+    }
+
+    // Method to hide icons
+    public void hideIcons() {
+        playerIcon.setVisible(false);
+        enemyIcon.setVisible(false);
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
